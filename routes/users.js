0f9ddf72443b238e0be814df1ebcea89b4ecd41d
feng <2995207228@qq.com>
2017-07-04 10:48:08 +0800

    完成了商品列表

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,6 @@ router.post('/login', async function (ctx, next) {
   let { User } = ctx.models
   let { body } = ctx.request
   let _user = await User.findUsersByName(body.name)
-  
   ctx.assert(_user.length > 0, 503, {
     responseData: ['name', '用户名错误', 'auth'], isShow: true
   })
