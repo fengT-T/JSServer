43397542bfac3bdb5c9a1ccb15eabc7c6b3897c3
feng <2995207228@qq.com>
2017-07-06 21:01:34 +0800

    上架

---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const router = require('koa-router')()
+
+router.prefix('/admin')
+
+router.use('/', async (ctx, next) => {
+  ctx.assert(ctx.session.user.isAdmin, 401)
+  await next()
+})
+
+router.post('/modifyGoods', async function (ctx, next) {
+  let { Goods } = ctx.models
+  let body = ctx.request.body
+  let goods = await Goods.findByIdAndUpdate(body.id, body, {
+    new: true, // 很坑的一个东西，返回新的，不然默认返回旧的
+    runValidators: true// 一定要验证不然完全gg
+  }).exec()
+  ctx.body = goods
+})
+
+module.exports = router
--- routes/goods.js
+++ routes/goods.js
@@ -2,6 +2,14 @@ const router = require('koa-router')()
 
 router.prefix('/goods')
 
+router.get('/all', async function (ctx, next) {
+  let { Goods } = ctx.models
+  let list = await Goods.find({})
+    .sort({ '_id': -1 })
+    .exec()
+  ctx.body = list
+})
+
 router.get('/list', async function (ctx, next) {
   let { Goods } = ctx.models
   let { startId = await Goods.getFirstId() } = ctx.query
@@ -20,18 +28,6 @@ router.get('/info', async function (ctx, next) {
   let { Goods } = ctx.models
   let { id } = ctx.query
   let goods = await Goods.findById(id).exec()
-  ctx.body = goods
-})
-
-router.post('/modify', async function (ctx, next) {
-  let { Goods } = ctx.models
-  let { user } = ctx.session
-  let body = ctx.request.body
-  ctx.assert(user.isAdmin, 401)
-  let goods = await Goods.findByIdAndUpdate(body.id, body, {
-    new: true, // 很坑的一个东西，返回新的，不然默认返回旧的
-    runValidators: true// 一定要验证不然完全gg
-  }).exec()
   ctx.body = goods
 })
 
